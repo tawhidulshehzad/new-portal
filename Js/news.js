f997d3e694a5f4015c8f034b140d5b11a12b2880
shehzad1212 <tawhidulislam261@gmail.com>
2022-09-03 12:47:24 +0600

    category done

--- Js/news.js
+++ Js/news.js
@@ -2,5 +2,44 @@ const loadNews = () => {
   url = `https://openapi.programming-hero.com/api/news/categories`;
   fetch(url)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => displayCategory(data.data.news_category));
 };
+
+const displayCategory = (categories) => {
+  const categoryContainer = document.getElementById("category-container");
+  for (const category of categories) {
+    console.log(category);
+    const categoryDiv = document.createElement("div");
+    categoryDiv.innerHTML = `
+          <div class="text-center">
+            <button class="ctg-btn bg-white fs-5 px-3">${category.category_name}</button>
+          </div>
+    `;
+    categoryContainer.appendChild(categoryDiv);
+  }
+};
+
+loadNews();
+
+
+
+
+
+
+
+
+
+
+// const displayCategory = (categories) => {
+//   const categoryContainer = document.getElementById("category-container");
+//   console.log(categories);
+//   const categoryDiv = document.createElement("div");
+//   categoryDiv.innerHTML = `
+//           <button class="ctg-btn bg-white fs-5 px-3">${categories[0].category_name}</button>
+//           <button class="ctg-btn bg-white fs-5 px-3">${categories[1].category_name}</button>
+
+//     `;
+//   categoryContainer.appendChild(categoryDiv);
+// };
+
+// loadNews();
