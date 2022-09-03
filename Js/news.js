7e0578a521fc42c6d2422bd11ef0b134a7238621
shehzad1212 <tawhidulislam261@gmail.com>
2022-09-03 18:26:40 +0600

    news update

--- Js/news.js
+++ Js/news.js
@@ -1,9 +1,11 @@
+// load categories
 const loadCategory = () => {
   url = `https://openapi.programming-hero.com/api/news/categories`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => displayCategory(data.data.news_category));
 };
+// display categories
 
 const displayCategory = (categories) => {
   const categoryContainer = document.getElementById("category-container");
@@ -11,24 +13,104 @@ const displayCategory = (categories) => {
     // console.log(category);
     const categoryDiv = document.createElement("div");
     categoryDiv.innerHTML = `
-            <button onclick="loadNews(${category.category_id})" class="ctg-btn bg-white fs-5 px-3">${category.category_name}</button>
+            <button  onclick="loadNews('${category.category_id}')" class="ctg-btn bg-white fs-5 px-3">${category.category_name}</button>
           
     `;
     categoryContainer.appendChild(categoryDiv);
   }
 };
+// item count
 
+const getItemsCount = (items) => {
+  const itemContainer = document.getElementById("itemsFound-Container");
+  itemContainer.innerHTML = "";
+  const itemDiv = document.createElement("div");
+  itemDiv.innerHTML = `
+              <div class="fs-6 item-found px-3 ">
+                <h3 class="fw-light p-2">${items} Items Found for this category</h3>
+              </div>
+            
+      `;
+  itemContainer.appendChild(itemDiv);
+};
+
+// Color change
+
+function DisableNextButton(btnId) {
+  const disableButton = document.getElementById(btnId);
+  document.getElementById(btnId).disabled = "true";
+  disableButton.style.background = "gray";
+}
+
+// Load News
 const loadNews = (categoryId) => {
   url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
   fetch(url)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => displayNews(data.data));
+};
+// display News
+const displayNews = (bulletins) => {
+  getItemsCount(bulletins.length);
+  const newsContainer = document.getElementById("news-container");
+  newsContainer.innerHTML = "";
+  for (const bulletin of bulletins) {
+    console.log(bulletin);
+    const newsDiv = document.createElement("div");
+    newsDiv.innerHTML = `
+    <div class="d-flex m-2 p-2 shadow-sm ">
+      <div class=" col-3">
+        <img class="w-75" src="${bulletin.thumbnail_url}" alt="" />
+      </div>
+      <div class="col-9">
+        <div>
+          <div>
+            <h1 class="fs-5">${bulletin.title}</h1>
+            <p class= "news-details">
+            ${bulletin.details}
+            </p>
+          </div>
+          <div class="d-flex justify-content-between px-2">
+            <div class="d-flex">
+              <div class="right-img-container">
+                <img class="w-100 raounded-5" src="${bulletin.author.img}" alt="" />
+              </div>
+              <div>
+                <h6 class="mb-0">${bulletin.author.name}</h6>
+                <p>${bulletin.author.published_date}</p>
+              </div>
+            </div>
+            <div>
+              <div class="d-flex gap-1 mt-2">
+              <p><i class="fa fa-eye" aria-hidden="true"></i></p>
+                <p>${bulletin.total_view}</p>
+              </div>
+            </div>
+            <div>
+              <button id="btn-details" data-bs-toggle="modal"
+              data-bs-target="#newsModal" > Details</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    `;
+    newsContainer.appendChild(newsDiv);
+  }
 };
 
-const displayNews = () => {};
+
+const modalShown = () => {
+  
+}
+
+
 
 loadCategory();
+loadNews("04");
 
+//
 // const displayCategory = (categories) => {
 //   const categoryContainer = document.getElementById("category-container");
 //   console.log(categories);
