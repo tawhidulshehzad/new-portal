bbcf2ed993824a1d877996ccf7a6e992421e5422
shehzad1212 <tawhidulislam261@gmail.com>
2022-09-03 20:54:37 +0600

    modal creation

--- Js/news.js
+++ Js/news.js
@@ -9,15 +9,14 @@ const loadCategory = () => {
 
 const displayCategory = (categories) => {
   const categoryContainer = document.getElementById("category-container");
-  for (const category of categories) {
-    // console.log(category);
+  categories.forEach((category) => {
     const categoryDiv = document.createElement("div");
     categoryDiv.innerHTML = `
             <button  onclick="loadNews('${category.category_id}')" class="ctg-btn bg-white fs-5 px-3">${category.category_name}</button>
           
     `;
     categoryContainer.appendChild(categoryDiv);
-  }
+  });
 };
 // item count
 
@@ -26,8 +25,8 @@ const getItemsCount = (items) => {
   itemContainer.innerHTML = "";
   const itemDiv = document.createElement("div");
   itemDiv.innerHTML = `
-              <div class="fs-6 item-found px-3 ">
-                <h3 class="fw-light p-2">${items} Items Found for this category</h3>
+              <div class=" px-3 ">
+                <h3 class=" p-2">${items} Items Found for this category</h3>
               </div>
             
       `;
@@ -36,14 +35,15 @@ const getItemsCount = (items) => {
 
 // Color change
 
-function DisableNextButton(btnId) {
-  const disableButton = document.getElementById(btnId);
-  document.getElementById(btnId).disabled = "true";
-  disableButton.style.background = "gray";
-}
+// function DisableNextButton(btnId) {
+//   const disableButton = document.getElementById(btnId);
+//   document.getElementById(btnId).disabled = "true";
+//   disableButton.style.background = "gray";
+// }
 
 // Load News
 const loadNews = (categoryId) => {
+  toggleSpinner(true);
   url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
   fetch(url)
     .then((response) => response.json())
@@ -87,7 +87,7 @@ const displayNews = (bulletins) => {
               </div>
             </div>
             <div>
-              <button id="btn-details" data-bs-toggle="modal"
+              <button onclick="modalShown('${bulletin._id}')" id="btn-details" data-bs-toggle="modal"
               data-bs-target="#newsModal" > Details</button>
             </div>
           </div>
@@ -98,17 +98,46 @@ const displayNews = (bulletins) => {
     `;
     newsContainer.appendChild(newsDiv);
   }
+  toggleSpinner(false);
 };
 
+const modalShown = (detailsId) => {
+  url = `https://openapi.programming-hero.com/api/news/${detailsId}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => modalDisplay(data.data));
+};
 
-const modalShown = () => {
-  
-}
+const modalDisplay = (modalData) => {
+  console.log(modalData[0].details);
+  const modalTitle = document.getElementById("NewsTitleModal");
+  modalTitle.innerText = modalData[0].title;
+  const modalBody = document.getElementById("newsModalBody");
+  modalBody.innerHTML = `
+  <p> ${modalData[0].details ? modalData[0].details : "Not found"}</p>
+  <p>Author : ${
+    modalData[0].author ? modalData[0].author.name : "Not Available"
+  }</p>
+  <p>Published : ${
+    modalData[0].author ? modalData[0].author.published_date : "not found"
+  }</p>
+  <p>Total view: ${
+    modalData[0].total_view ? modalData[0].total_view : "Total not found"
+  }</p>
+  `;
+};
 
-
+const toggleSpinner = (isLoading) => {
+  const loderSections = document.getElementById("spinner");
+  if (isLoading === true) {
+    loderSections.classList.remove("d-none");
+  } else {
+    loderSections.classList.add("d-none");
+  }
+};
 
 loadCategory();
-loadNews("04");
+// loadNews("04");
 
 //
 // const displayCategory = (categories) => {
